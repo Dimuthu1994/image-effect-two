36598b9d8f522b8f5df05e2965e55d5201fdd55e
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-21 08:00:18 +0530

    grayscalin an image

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,9 @@ log(&"Grayscale called".into());
 let base64_to_vector = decode(encoded_file).unwrap();
 log(&"Image decoded".into());
 
-let img = load_from_memory(&base64_to_vector).unwrap();
+let mut img = load_from_memory(&base64_to_vector).unwrap();
 log(&"Image loaded".into());
+
+img = img.grayscale();
+log(&"Grayscale effect applied".into());
 }
