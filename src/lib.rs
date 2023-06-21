6a91b1e0f1bd110c576cddd69457fa80a910fdf6
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-21 08:06:44 +0530

    buffering an image

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 use base64::decode;
 use image::load_from_memory;
+use image::ImageOutputFormat::Png;
+
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str){
@@ -15,4 +17,8 @@ log(&"Image loaded".into());
 
 img = img.grayscale();
 log(&"Grayscale effect applied".into());
+
+let mut buffer = vec![];
+img.write_to(&mut buffer, Png).unwrap();
+log(&"New Image Written".into());
 }
